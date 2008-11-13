f577462dfeacb01c20ea2c8ef7ffabb9da3d7624
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2008-11-13 04:06:31 +0000

    [maven-release-plugin]  copy for tag mycila-plugin-1.1

---
+++ mycila-plugin/tags/mycila-plugin-1.1/src/main/java/com/mycila/plugin/api/PluginCreationException.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (C) 2008 Mathieu Carbou <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mycila.plugin.api;
+
+import static java.lang.String.*;
+import java.net.URL;
+
+/**
+ * @author Mathieu Carbou (mathieu.carbou@gmail.com)
+ */
+public final class PluginCreationException extends PluginException {
+    private static final long serialVersionUID = 2974368486006410596L;
+    private final URL descriptor;
+    private final String plugin;
+    private final String clazz;
+    private final Class<?> pluginType;
+
+    public PluginCreationException(String message, URL descriptor, String name, String clazz, Class<?> pluginsType, Throwable e) {
+        super(format("%s: %s\n%s", message, e.getMessage(), info(descriptor, name, clazz, pluginsType)), e);
+        this.descriptor = descriptor;
+        this.plugin = name;
+        this.clazz = clazz;
+        this.pluginType = pluginsType;
+    }
+
+    public PluginCreationException(String message, URL descriptor, String name, String clazz, Class<?> pluginsType) {
+        super(format("%s\n%s", message, info(descriptor, name, clazz, pluginsType)));
+        this.descriptor = descriptor;
+        this.plugin = name;
+        this.clazz = clazz;
+        this.pluginType = pluginsType;
+    }
+
+    public String getClazz() {
+        return clazz;
+    }
+
+    public URL getDescriptor() {
+        return descriptor;
+    }
+
+    public String getPlugin() {
+        return plugin;
+    }
+
+    public Class<?> getPluginType() {
+        return pluginType;
+    }
+
+    private static String info(URL descriptor, String name, String clazz, Class<?> pluginsType) {
+        return new StringBuilder()
+                .append("- plugins descriptor: ").append(descriptor).append("\n")
+                .append("- plugins type: ").append(pluginsType.getName()).append("\n")
+                .append("- plugin name: ").append(name).append("\n")
+                .append("- plugin class: ").append(clazz)
+                .toString();
+    }
+
+}
