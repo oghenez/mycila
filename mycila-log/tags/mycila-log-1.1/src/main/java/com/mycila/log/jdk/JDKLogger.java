ae120f6932209ae92d21d9dc4efd1aacaf4bb1b4
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2009-04-11 15:53:34 +0000

    [maven-release-plugin]  copy for tag mycila-log-1.1

---
+++ mycila-log/tags/mycila-log-1.1/src/main/java/com/mycila/log/jdk/JDKLogger.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (C) 2008 Mathieu Carbou <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mycila.log.jdk;
+
+import com.mycila.log.AbstractLogger;
+
+import java.text.MessageFormat;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+/**
+ * @author Mathieu Carbou (mathieu.carbou@gmail.com)
+ */
+public final class JDKLogger extends AbstractLogger {
+
+    private final java.util.logging.Logger logger;
+
+    public JDKLogger(String name) {
+        this.logger = java.util.logging.Logger.getLogger(name);
+    }
+
+    public boolean canLog(com.mycila.log.Level level) {
+        switch (level) {
+            case TRACE:
+                return logger.isLoggable(Level.FINEST);
+            case DEBUG:
+                return logger.isLoggable(Level.FINE);
+            case INFO:
+                return logger.isLoggable(Level.INFO);
+            case WARN:
+                return logger.isLoggable(Level.WARNING);
+            case ERROR:
+                return logger.isLoggable(Level.SEVERE);
+            default:
+                return false;
+        }
+    }
+
+    protected void doLog(com.mycila.log.Level level, Throwable throwable, String message, Object... args) {
+        switch (level) {
+            case TRACE:
+                logger.log(buildLogRecord(Level.FINEST, throwable, message, args));
+                break;
+            case DEBUG:
+                logger.log(buildLogRecord(Level.FINE, throwable, message, args));
+                break;
+            case INFO:
+                logger.log(buildLogRecord(Level.INFO, throwable, message, args));
+                break;
+            case WARN:
+                logger.log(buildLogRecord(Level.WARNING, throwable, message, args));
+                break;
+            case ERROR:
+                logger.log(buildLogRecord(Level.SEVERE, throwable, message, args));
+                break;
+            default:
+
+        }
+    }
+
+    private LogRecord buildLogRecord(Level level, Throwable throwable, String message, Object... args) {
+        LogRecord logRecord = new LogRecord(level, MessageFormat.format(message, args));
+        logRecord.setLoggerName(logger.getName());
+        logRecord.setSourceClassName(logger.getName());
+        logRecord.setSourceMethodName(Thread.currentThread().getName());
+        logRecord.setThrown(throwable);
+        return logRecord;
+    }
+}
