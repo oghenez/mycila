783b1723c07dd283214f573410f968d207c465ce
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2009-08-02 23:18:46 +0000

    [maven-release-plugin]  copy for tag mycila-testing-2.1

---
+++ mycila-testing/tags/mycila-testing-2.1/mycila-testing-api/src/main/java/com/mycila/testing/core/introspect/Introspector.java
@@ -0,0 +1,144 @@
+/**
+ * Copyright (C) 2008 Mathieu Carbou <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mycila.testing.core.introspect;
+
+import com.mycila.testing.MycilaTestingException;
+import static com.mycila.testing.core.api.Ensure.*;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Handles a test instance
+ *
+ * @author Mathieu Carbou (mathieu.carbou@gmail.com)
+ */
+public final class Introspector {
+
+    private final Object instance;
+
+    public Introspector(Object instance) {
+        notNull("Object to introspect", instance);
+        this.instance = instance;
+    }
+
+    /**
+     * @return The test instance
+     */
+    public Object instance() {
+        return instance;
+    }
+
+    /**
+     * @return The test instance's class
+     */
+    public Class<?> testClass() {
+        return instance.getClass();
+    }
+
+    /**
+     * Select some fields. Use the {@link Filters} class to create and compose filters
+     *
+     * @param filter Filter to use for selection
+     * @return A list containing the selected objects
+     */
+    public List<Field> selectFields(Filter<Field> filter) {
+        notNull("Filter", filter);
+        for (Class<?> c = testClass(); c != Object.class; c = c.getSuperclass()) {
+            for (Field field : c.getDeclaredFields()) {
+                filter.add(accessible(field));
+            }
+        }
+        return filter.select();
+    }
+
+    /**
+     * Select some methods. Use the {@link Filters} class to create and compose filters
+     *
+     * @param filter Filter to use for selection
+     * @return A list containing the selected objects
+     */
+    public List<Method> selectMethods(Filter<Method> filter) {
+        notNull("Filter", filter);
+        for (Class<?> c = testClass(); c != Object.class; c = c.getSuperclass()) {
+            for (Method method : c.getDeclaredMethods()) {
+                filter.add(accessible(method));
+            }
+        }
+        return filter.select();
+    }
+
+    /**
+     * Invoke a method on this test instance
+     *
+     * @param method Method to be invoked
+     * @param args   Method argument
+     * @return The result of the invokation
+     */
+    public Object invoke(Method method, Object... args) {
+        notNull("Method", method);
+        notNull("Method argumentd", args);
+        try {
+            return method.invoke(instance(), args);
+        } catch (IllegalAccessException e) {
+            throw new MycilaTestingException(e, "Error invoking method '%s' with arguments: %s: %s", method, Arrays.deepToString(args), e.getMessage());
+        } catch (InvocationTargetException e) {
+            throw new MycilaTestingException(e.getTargetException().getClass().getSimpleName() + ": " + e.getTargetException().getMessage(), e.getTargetException());
+        } catch (Exception e) {
+            throw new MycilaTestingException(e, "Error invoking method '%s' with arguments: %s: %s", method, Arrays.deepToString(args), e.getMessage());
+        }
+    }
+
+    /**
+     * Get a field's value
+     *
+     * @param field The field
+     * @return The field value
+     */
+    public Object get(Field field) {
+        notNull("Field", field);
+        try {
+            return field.get(instance());
+        } catch (Exception e) {
+            throw new MycilaTestingException(e, "Error getting value of field '%s' on test class '%s': %s", field, testClass().getName(), e.getMessage());
+        }
+    }
+
+    /**
+     * Set a field value
+     *
+     * @param field The field
+     * @param value The new field value
+     */
+    public void set(Field field, Object value) {
+        notNull("Field", field);
+        try {
+            field.set(instance(), value);
+        } catch (Exception e) {
+            throw new MycilaTestingException(e, "Error setting value on field '%s' on test class '%s': %s", field, testClass().getName(), e.getMessage());
+        }
+    }
+
+    public boolean hasAnnotation(Class<? extends Annotation> annot) {
+        notNull("Annotation", annot);
+        return testClass().isAnnotationPresent(annot);
+    }
+
+}
---
+++ mycila-testing/tags/mycila-testing-2.1/mycila-testing-api/src/test/java/com/mycila/testing/ea/ExtendedAssertTest.java
@@ -0,0 +1,228 @@
+/**
+ * Copyright (C) 2008 Mathieu Carbou <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mycila.testing.ea;
+
+import static com.mycila.testing.ea.ExtendedAssert.*;
+import static org.testng.Assert.*;
+import static org.testng.Assert.fail;
+import org.testng.annotations.Test;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @author Mathieu Carbou (mathieu.carbou@gmail.com)
+ */
+public final class ExtendedAssertTest {
+
+    @Test
+    public void test_assertNotEquals() throws Exception {
+        assertNotEquals("", null);
+        assertNotEquals(null, "");
+        assertNotEquals(new Object(), new Object());
+        try {
+            assertNotEquals(null, null);
+            fail();
+        } catch (AssertionError e) {
+            assertEquals(e.getMessage(), "expected:<Objects not equals> but was:<null>");
+        }
+        try {
+            assertNotEquals("aa", "aa");
+            fail();
+        } catch (AssertionError e) {
+            assertEquals(e.getMessage(), "expected:<Objects not equals> but was:<aa>");
+        }
+    }
+
+    @Test
+    public void test_assertEmpty() throws Exception {
+        assertBlank(null);
+        assertBlank("");
+        assertBlank("  ");
+        assertEmpty((String) null);
+        assertEmpty("");
+        assertEmpty((Object[]) null);
+        assertEmpty(new Object[0]);
+        assertEmpty((Collection<?>) null);
+        assertEmpty(Collections.emptySet());
+    }
+
+    @Test
+    public void test_assertThrow_exception() throws Exception {
+
+        // ok
+        assertThrow(RuntimeException.class).whenRunning(new Code() {
+            public void run() throws Throwable {
+                throw new RuntimeException("hello");
+            }
+        });
+
+        // no exception thrown when expected
+        try {
+            assertThrow(RuntimeException.class).whenRunning(new Code() {
+                public void run() throws Throwable {
+                }
+            });
+            fail();
+        } catch (AssertionError e) {
+            e.printStackTrace();
+        }
+
+        // bad exception thrown
+        try {
+            assertThrow(IllegalArgumentException.class).whenRunning(new Code() {
+                public void run() throws Throwable {
+                    throw new IllegalStateException();
+                }
+            });
+            fail();
+        } catch (AssertionError e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test_assertThrow_exception_withMessage() throws Exception {
+
+        // ok
+        assertThrow(RuntimeException.class).withMessage("hello").whenRunning(new Code() {
+            public void run() throws Throwable {
+                throw new RuntimeException("hello");
+            }
+        });
+
+        // ok - null message
+        assertThrow(RuntimeException.class).withMessage(null).whenRunning(new Code() {
+            public void run() throws Throwable {
+                throw new RuntimeException();
+            }
+        });
+
+        // no exception thrown
+        try {
+            assertThrow(RuntimeException.class).withMessage("Hello !").whenRunning(new Code() {
+                public void run() throws Throwable {
+                }
+            });
+            fail();
+        } catch (AssertionError e) {
+            e.printStackTrace();
+        }
+
+        // message expected, got null
+        try {
+            assertThrow(RuntimeException.class).withMessage("hello").whenRunning(new Code() {
+                public void run() throws Throwable {
+                    throw new RuntimeException();
+                }
+            });
+            fail();
+        } catch (AssertionError e) {
+            e.printStackTrace();
+        }
+
+        // null expected, got message
+        try {
+            assertThrow(RuntimeException.class).withMessage(null).whenRunning(new Code() {
+                public void run() throws Throwable {
+                    throw new RuntimeException("dd");
+                }
+            });
+            fail();
+        } catch (AssertionError e) {
+            e.printStackTrace();
+        }
+
+        // bad message
+        try {
+            assertThrow(RuntimeException.class).withMessage("aa").whenRunning(new Code() {
+                public void run() throws Throwable {
+                    throw new RuntimeException("dd");
+                }
+            });
+            fail();
+        } catch (AssertionError e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void test_assertThrow_containingMessgae() throws Exception {
+
+        // ok
+        assertThrow(RuntimeException.class).containingMessage("hello").whenRunning(new Code() {
+            public void run() throws Throwable {
+                throw new RuntimeException("hello world");
+            }
+        });
+
+        // ok - null message
+        assertThrow(RuntimeException.class).containingMessage(null).whenRunning(new Code() {
+            public void run() throws Throwable {
+                throw new RuntimeException();
+            }
+        });
+
+        // no exception thrown
+        try {
+            assertThrow(RuntimeException.class).containingMessage("Hello !").whenRunning(new Code() {
+                public void run() throws Throwable {
+                }
+            });
+            fail();
+        } catch (AssertionError e) {
+            e.printStackTrace();
+        }
+
+        // message expected, got null
+        try {
+            assertThrow(RuntimeException.class).containingMessage("hello").whenRunning(new Code() {
+                public void run() throws Throwable {
+                    throw new RuntimeException();
+                }
+            });
+            fail();
+        } catch (AssertionError e) {
+            e.printStackTrace();
+        }
+
+        // null expected, got message
+        try {
+            assertThrow(RuntimeException.class).containingMessage(null).whenRunning(new Code() {
+                public void run() throws Throwable {
+                    throw new RuntimeException("dd");
+                }
+            });
+            fail();
+        } catch (AssertionError e) {
+            e.printStackTrace();
+        }
+
+        // bad message
+        try {
+            assertThrow(RuntimeException.class).containingMessage("aa").whenRunning(new Code() {
+                public void run() throws Throwable {
+                    throw new RuntimeException("dd");
+                }
+            });
+            fail();
+        } catch (AssertionError e) {
+            e.printStackTrace();
+        }
+    }
+
+}
