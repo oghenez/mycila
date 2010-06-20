f254a4d5cc9cc4aec8cdb11cd5abb65306d59f43
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2010-06-20 00:03:42 +0000

    [maven-release-plugin]  copy for tag mycila-event-3.5

---
+++ mycila-event/tags/mycila-event-3.5/src/main/java/com/mycila/event/spi/DefaultDispatcher.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (C) 2009 Mathieu Carbou <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mycila.event.spi;
+
+import com.mycila.event.api.Dispatcher;
+import com.mycila.event.api.ErrorHandler;
+import com.mycila.event.api.Event;
+import com.mycila.event.api.Subscriber;
+import com.mycila.event.api.Subscription;
+import com.mycila.event.api.topic.Topic;
+import com.mycila.event.api.topic.TopicMatcher;
+
+import java.util.Iterator;
+import java.util.concurrent.Executor;
+
+import static com.mycila.event.api.Ensure.*;
+
+/**
+ * @author Mathieu Carbou (mathieu.carbou@gmail.com)
+ */
+public class DefaultDispatcher implements Dispatcher {
+
+    private final SubscriptionManager subscriptionManager = new SubscriptionManager();
+    private final ErrorHandler errorHandler;
+    private final Executor publishExecutor;
+    private final Executor subscriberExecutor;
+
+    public DefaultDispatcher(ErrorHandler errorHandler,
+                      Executor publishExecutor,
+                      Executor subscriberExecutor) {
+        this.errorHandler = notNull(errorHandler, "ErrorHandler");
+        this.publishExecutor = notNull(publishExecutor, "Publishing executor");
+        this.subscriberExecutor = notNull(subscriberExecutor, "Subscriber executor");
+    }
+
+    public final <E> void publish(final Topic topic, final E source) {
+        notNull(topic, "Topic");
+        notNull(source, "Event source");
+        publishExecutor.execute(new Runnable() {
+            public void run() {
+                final Event<E> event = Events.event(topic, source);
+                final Iterator<Subscription<E>> subscriptionIterator = subscriptionManager.getSubscriptions(event);
+                while (subscriptionIterator.hasNext()) {
+                    final Subscription<E> subscription = subscriptionIterator.next();
+                    subscriberExecutor.execute(new Runnable() {
+                        public void run() {
+                            try {
+                                subscription.getSubscriber().onEvent(event);
+                            } catch (Exception e) {
+                                errorHandler.onError(subscription, event, e);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    public final <E> void subscribe(TopicMatcher matcher, Class<?> eventType, Subscriber<E> subscriber) {
+        notNull(matcher, "TopicMatcher");
+        notNull(eventType, "Event type");
+        notNull(subscriber, "Subscriber");
+        subscriptionManager.addSubscription(Subscriptions.create(matcher, eventType, subscriber));
+    }
+
+    public final <E> void unsubscribe(Subscriber<E> subscriber) {
+        notNull(subscriber, "Subscriber");
+        subscriptionManager.removeSubscriber(subscriber);
+    }
+
+    public void close() {
+    }
+
+}
---
+++ mycila-event/tags/mycila-event-3.5/src/test/java/com/mycila/event/spi/AnnotationProcessorTest.java
@@ -0,0 +1,116 @@
+/**
+ * Copyright (C) 2009 Mathieu Carbou <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mycila.event.spi;
+
+import com.mycila.event.api.Dispatcher;
+import com.mycila.event.api.ErrorHandlers;
+import com.mycila.event.api.Event;
+import com.mycila.event.api.annotation.AnnotationProcessor;
+import com.mycila.event.api.annotation.Publish;
+import com.mycila.event.api.annotation.Reference;
+import com.mycila.event.api.annotation.Subscribe;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mycila.event.api.Reachability.*;
+import static org.junit.Assert.*;
+
+/**
+ * @author Mathieu Carbou (mathieu.carbou@gmail.com)
+ */
+@RunWith(JUnit4.class)
+public final class AnnotationProcessorTest {
+
+    private final List<Object> sequence = new ArrayList<Object>();
+
+    AnnotationProcessor processor;
+    Dispatcher dispatcher;
+
+    @Before
+    public void setup() {
+        dispatcher = Dispatchers.synchronousUnsafe(ErrorHandlers.rethrow());
+        processor = AnnotationProcessors.create(dispatcher);
+        sequence.clear();
+    }
+
+    @Test
+    public void test_subscribe_strong() {
+        Object o = new Object() {
+            @Subscribe(topics = {"prog/events/a", "prog/events/**"}, eventType = String.class)
+            private void handle(Event<String> event) {
+                sequence.add(event.getSource());
+            }
+        };
+        processor.process(o);
+        publish();
+        assertEquals(sequence.toString(), "[Hello for a, Hello for a1, Hello for a1]");
+    }
+
+    @Test
+    public void test_subscribe_weak() {
+        Object o = new Object() {
+            @Subscribe(topics = {"prog/events/a", "prog/events/b/**"}, eventType = String.class)
+            @Reference(WEAK)
+            private void handle(Event<String> event) {
+                sequence.add(event.getSource());
+            }
+        };
+        processor.process(o);
+
+        System.gc();
+        System.gc();
+        System.gc();
+
+        publish();
+        assertEquals(sequence.toString(), "[]");
+    }
+
+    @Test
+    public void test_multiple_publish() {
+        Object o = new Object() {
+            @Subscribe(topics = "prog/events/a/**", eventType = String.class)
+            private void handle(Event<String> event) {
+                sequence.add(event.getSource());
+            }
+        };
+        processor.process(o);
+        publish();
+        assertEquals(sequence.toString(), "[Hello for a, Hello for a1, Hello for a1]");
+    }
+
+    private void publish() {
+        B b = processor.proxy(B.class);
+        C c = processor.proxy(C.class);
+        b.send("Hello for a", 1);
+        c.send("Hello for a1", 4);
+    }
+
+    private static interface B {
+        @Publish(topics = "prog/events/a")
+        void send(String a, int b);
+    }
+
+    static abstract class C {
+        @Publish(topics = {"prog/events/a/a1", "prog/events/a/allA"})
+        abstract void send(String a, int b);
+    }
+}
