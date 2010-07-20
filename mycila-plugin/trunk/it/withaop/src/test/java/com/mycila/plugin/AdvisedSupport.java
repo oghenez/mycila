dbbec1e0fa36484d475ab94bc46b0da30ad980a8
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2010-07-20 01:39:33 +0000

    refac

--- mycila-plugin/trunk/it/withaop/src/test/java/com/mycila/plugin/AdvisedSupport.java
+++ mycila-plugin/trunk/it/withaop/src/test/java/com/mycila/plugin/AdvisedSupport.java
@@ -1,11 +1,11 @@
-/*
- * Copyright 2002-2010 the original author or authors.
+/**
+ * Copyright (C) 2010 Mathieu Carbou <mathieu.carbou@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -609,17 +609,17 @@ public class AdvisedSupport extends ProxyConfig implements Advised {
 
         @Override
         public boolean equals(Object other) {
-			if (other == this) {
-				return true;
-			}
-			MethodCacheKey otherKey = (MethodCacheKey) other;
-			return (this.method == otherKey.method);
-		}
+            if (other == this) {
+                return true;
+            }
+            MethodCacheKey otherKey = (MethodCacheKey) other;
+            return (this.method == otherKey.method);
+        }
 
-		@Override
-		public int hashCode() {
-			return this.hashCode;
-		}
-	}
+        @Override
+        public int hashCode() {
+            return this.hashCode;
+        }
+    }
 
 }
