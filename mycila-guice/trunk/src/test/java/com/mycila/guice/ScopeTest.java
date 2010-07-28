935d631ecd58a01f8a380814d6f0ffdb12c7ef6b
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2010-07-28 04:03:22 +0000

    added lazy singleton scope

--- mycila-guice/trunk/src/test/java/com/mycila/guice/ScopeTest.java
+++ mycila-guice/trunk/src/test/java/com/mycila/guice/ScopeTest.java
@@ -18,12 +18,9 @@ package com.mycila.guice;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
-import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.Provider;
-import com.google.inject.Stage;
-import com.mycila.guice.annotation.ConcurrentSingleton;
 import com.mycila.guice.annotation.Expirity;
 import com.mycila.guice.annotation.RenewableSingleton;
 import com.mycila.guice.annotation.SoftSingleton;
@@ -33,8 +30,6 @@ import org.junit.runners.JUnit4;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
-import java.util.concurrent.TimeUnit;
-
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
@@ -43,56 +38,6 @@ import static org.mockito.Mockito.*;
  */
 @RunWith(JUnit4.class)
 public final class ScopeTest {
-
-    public static void main(String... args) {
-        long start = System.nanoTime();
-        Injector injector = Guice.createInjector(Stage.PRODUCTION, new AbstractModule() {
-            public void configure() {
-                ExtraScope.installAll(binder());
-                bind(C.class);
-                bind(D.class);
-            }
-        });
-        injector.getInstance(A.class);
-        long elapsed = System.nanoTime() - start;
-        System.out.printf("Completed in %d seconds%n", TimeUnit.NANOSECONDS.toMillis(elapsed));
-        assertTrue(TimeUnit.NANOSECONDS.toMillis(elapsed) < 5000);
-    }
-
-    @Test
-    public void test_threads_stopping_if_injector_garbadgedcollected() throws Exception {
-        System.out.println("create inj.");
-        Injector injector = Guice.createInjector(Stage.PRODUCTION, new AbstractModule() {
-            public void configure() {
-                ExtraScope.installAll(binder());
-                bind(Solo.class);
-            }
-        });
-        System.out.println("get Solo");
-        injector.getInstance(Solo.class);
-        injector = null;
-        System.out.println("gc");
-        System.gc();
-        System.gc();
-        System.out.println("wait...");
-        Thread.sleep(5000);
-    }
-
-    @Test
-    public void test_concurrent() throws Exception {
-        long start = System.nanoTime();
-        Injector injector = Guice.createInjector(Stage.DEVELOPMENT, new AbstractModule() {
-            public void configure() {
-                ExtraScope.installAll(binder());
-                bind(C.class);
-                bind(D.class);
-            }
-        });
-        injector.getInstance(A.class);
-        long elapsed = System.nanoTime() - start;
-        System.out.printf("Completed in %d seconds%n", TimeUnit.NANOSECONDS.toMillis(elapsed));
-        assertTrue(TimeUnit.NANOSECONDS.toMillis(elapsed) < 5000);
-    }
 
     @Test
     public void test_eq() throws Exception {
@@ -204,86 +149,6 @@ public final class ScopeTest {
         assertSame(o1, o2);
         Thread.sleep(600);
         assertNotSame(o1, injector.getInstance(Key.get(Object.class, ExtraScope.expirity(500))));
-    }
-
-    @ConcurrentSingleton
-    static class A {
-        @Inject
-        public A(Provider<B> b, Provider<C> c) {
-            try {
-                System.out.printf("Starting A on thread %s%n", Thread.currentThread().getName());
-                TimeUnit.SECONDS.sleep(1);
-                System.out.printf("A getting B and C instances on thread %s%n", Thread.currentThread().getName());
-                b.get();
-                c.get();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            } finally {
-                System.out.println("Started A");
-            }
-        }
-    }
-
-    @ConcurrentSingleton
-    static class B {
-        @Inject
-        public B() {
-            try {
-                System.out.printf("Starting B on thread %s%n", Thread.currentThread().getName());
-                TimeUnit.SECONDS.sleep(2);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            } finally {
-                System.out.println("Started B");
-            }
-        }
-    }
-
-    @ConcurrentSingleton
-    static class C {
-        @Inject
-        public C(Provider<D> d) {
-            try {
-                System.out.printf("Starting C on thread %s%n", Thread.currentThread().getName());
-                TimeUnit.SECONDS.sleep(3);
-                System.out.printf("C getting D instance on thread %s%n", Thread.currentThread().getName());
-                d.get();
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            } finally {
-                System.out.println("Started C");
-            }
-        }
-    }
-
-    @ConcurrentSingleton
-    static class D {
-        @Inject
-        public D() {
-            try {
-                System.out.printf("Starting D on thread %s%n", Thread.currentThread().getName());
-                TimeUnit.SECONDS.sleep(4);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            } finally {
-                System.out.println("Started D");
-            }
-        }
-    }
-
-    @ConcurrentSingleton
-    static class Solo {
-        @Inject
-        public Solo() {
-            try {
-                System.out.printf("Starting Solo on thread %s%n", Thread.currentThread().getName());
-                TimeUnit.SECONDS.sleep(1);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            } finally {
-                System.out.println("Started Solo");
-            }
-        }
     }
 
 }
