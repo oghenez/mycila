76c3b587ab95532a6ca5014f19e27cb81fb7037a
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2010-09-28 04:49:28 +0000

    jaxspot

---
+++ mycila-jdbc/trunk/src/main/java/com/mycila/jdbc/Update.java
@@ -0,0 +1,70 @@
+package com.mycila.jdbc;
+
+import net.playtouch.jaxspot.repository.sql.SqlException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public final class Update extends Request<Update> {
+
+    private PreparedStatement statement;
+
+    private Update(Connection connection, String query, String... columnNames) {
+        try {
+            statement = connection.prepareStatement(query, columnNames);
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public int execute() throws SqlException {
+        try {
+            return statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        } finally {
+            JdbcUtils.closeStatement(statement);
+        }
+    }
+
+    public Results executeAndReturnKeys() throws SqlException {
+        try {
+            statement.executeUpdate();
+            return Results.build(statement.getGeneratedKeys(), mapper);
+
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        } finally {
+            JdbcUtils.closeStatement(statement);
+        }
+    }
+
+    @Override
+    PreparedStatement getStatement() {
+        return statement;
+    }
+
+    public static final class Builder {
+
+        private final String query;
+        private final Connection connection;
+
+        Builder(Connection connection, String query) {
+            this.query = query;
+            this.connection = connection;
+        }
+
+        /**
+         * Execute the mutation and returns the values of the columns containing
+         * auto-generated data
+         */
+        public Update returning(String... columnNames) {
+            return new Update(connection, query, columnNames);
+        }
+
+        public Update noreturn() {
+            return new Update(connection, query);
+        }
+    }
+}
