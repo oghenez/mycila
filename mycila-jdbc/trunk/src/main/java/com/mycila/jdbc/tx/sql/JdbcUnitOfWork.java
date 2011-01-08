d964df61bf320c9a4bd292520d844c6821a57f1a
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2011-01-08 14:35:29 +0000

    added log traces

--- mycila-jdbc/trunk/src/main/java/com/mycila/jdbc/tx/sql/JdbcUnitOfWork.java
+++ mycila-jdbc/trunk/src/main/java/com/mycila/jdbc/tx/sql/JdbcUnitOfWork.java
@@ -20,20 +20,36 @@ import com.mycila.jdbc.UnitOfWork;
 
 import javax.inject.Singleton;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Singleton
 public final class JdbcUnitOfWork implements UnitOfWork {
+
+    private static final Logger LOGGER = Logger.getLogger(JdbcUnitOfWork.class.getName());
+
     @Override
     public void begin() {
+        // be sure to start with a brand new thread local map
+        ConnectionHolder.clean();
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.fine("Beginning Unit of Work");
     }
 
     @Override
     public void end() {
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.fine("Ending Unit of Work");
         for (ConnectionHolder holder : ConnectionHolder.listAll())
-            if (holder != null && holder.hasConnection())
+            if (holder != null && holder.hasConnection()) {
+                if (LOGGER.isLoggable(Level.FINE))
+                    LOGGER.fine("Closing JDBC connection");
                 try {
                     holder.getConnection().close();
                 } catch (SQLException ignored) {
                 }
+            }
+        // be sure to clean the thread local map
+        ConnectionHolder.clean();
     }
 }
