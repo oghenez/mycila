ee67efcfc734e0da88aeae14aa81657bcbad61c7
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2011-01-12 20:16:17 +0000

    [maven-release-plugin]  copy for tag mycila-jdbc-1.0.ga

---
+++ mycila-jdbc/tags/mycila-jdbc-1.0.ga/src/main/java/com/mycila/jdbc/query/Request.java
@@ -0,0 +1,219 @@
+/**
+ * Copyright (C) 2010 Mycila <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mycila.jdbc.query;
+
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Time;
+import java.sql.Timestamp;
+
+abstract class Request<T extends Request> {
+
+    protected final Mapper mapper = new Mapper();
+
+    public final T setNull(int parameterIndex) {
+        try {
+            getStatement().setNull(parameterIndex, SqlType.NULL.sqlType());
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setBoolean(int parameterIndex, boolean x) {
+        try {
+            getStatement().setBoolean(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setByte(int parameterIndex, byte x) {
+        try {
+            getStatement().setByte(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setShort(int parameterIndex, short x) {
+        try {
+            getStatement().setShort(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setInt(int parameterIndex, int x) {
+        try {
+            getStatement().setInt(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setLong(int parameterIndex, long x) {
+        try {
+            getStatement().setLong(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setFloat(int parameterIndex, float x) {
+        try {
+            getStatement().setFloat(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setDouble(int parameterIndex, double x) {
+        try {
+            getStatement().setDouble(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setBigDecimal(int parameterIndex, BigDecimal x) {
+        try {
+            getStatement().setBigDecimal(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setString(int parameterIndex, String x) {
+        try {
+            getStatement().setString(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setBytes(int parameterIndex, byte[] x) {
+        try {
+            getStatement().setBytes(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setDate(int parameterIndex, Date x) {
+        try {
+            getStatement().setDate(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setTime(int parameterIndex, Time x) {
+        try {
+            getStatement().setTime(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setTimestamp(int parameterIndex, Timestamp x) {
+        try {
+            getStatement().setTimestamp(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setObject(int parameterIndex, Object x) {
+        try {
+            getStatement().setObject(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public T setObjects(int parameterIndex, Object[] x) {
+        try {
+            getStatement().setObject(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setBlob(int parameterIndex, Blob x) {
+        try {
+            getStatement().setBlob(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setClob(int parameterIndex, Clob x) {
+        try {
+            getStatement().setClob(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public final T setURL(int parameterIndex, URL x) {
+        try {
+            getStatement().setURL(parameterIndex, x);
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public T clearParameters() {
+        try {
+            getStatement().clearParameters();
+            return (T) this;
+        } catch (SQLException e) {
+            throw new SqlException(e.getMessage(), e);
+        }
+    }
+
+    public <E> T convert(Class<E> type, Converter<E> converter) {
+        mapper.customConverters.put(type, converter);
+        return (T) this;
+    }
+
+    abstract PreparedStatement getStatement();
+}
---
+++ mycila-jdbc/tags/mycila-jdbc-1.0.ga/src/main/java/com/mycila/jdbc/tx/sql/JdbcTransactionManager.java
@@ -0,0 +1,152 @@
+/**
+ * Copyright (C) 2010 Mycila <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mycila.jdbc.tx.sql;
+
+import com.mycila.jdbc.tx.AbstractTransactionManager;
+import com.mycila.jdbc.tx.CannotCreateTransactionException;
+import com.mycila.jdbc.tx.SuspendableResource;
+import com.mycila.jdbc.tx.TransactionDefinition;
+import com.mycila.jdbc.tx.TransactionException;
+import com.mycila.jdbc.tx.TransactionSystemException;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Singleton
+public final class JdbcTransactionManager extends AbstractTransactionManager<TransactedObject> {
+
+    private static final Logger LOGGER = Logger.getLogger(JdbcTransactionManager.class.getName());
+
+    private final DataSource dataSource;
+
+    @Inject
+    public JdbcTransactionManager(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected TransactedObject doGetCurrent() {
+        return new TransactedObject().reusingConnectionHolder(ConnectionHolder.find(dataSource));
+    }
+
+    @Override
+    protected boolean hasActiveTransaction(TransactedObject transactedResource) {
+        boolean b = transactedResource.hasConnectionHolder() && transactedResource.getConnectionHolder().isTransactionActive();
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.fine("hasActiveTransaction: " + b);
+        return b;
+    }
+
+    @Override
+    protected void doBegin(TransactedObject transactedObject, TransactionDefinition definition) throws TransactionException {
+        Connection con = null;
+        try {
+            if (!transactedObject.hasConnectionHolder()) {
+                if (LOGGER.isLoggable(Level.FINE))
+                    LOGGER.fine("doBegin: Getting new JDBC connection");
+                transactedObject.newConnectionHolder(new ConnectionHolder(dataSource.getConnection()));
+            } else {
+                if (LOGGER.isLoggable(Level.FINE))
+                    LOGGER.fine("doBegin: Reusing previous JDBC connection");
+            }
+
+            con = transactedObject.getConnectionHolder().getConnection();
+
+            Integer previousIsolationLevel = DataSourceUtils.prepareConnectionForTransaction(con, definition);
+            transactedObject.setPreviousIsolationLevel(previousIsolationLevel);
+
+            // Switch to manual commit if necessary. This is very expensive in some JDBC drivers,
+            // so we don't want to do it unnecessarily (for example if we've explicitly
+            // configured the connection pool to set it already).
+            if (con.getAutoCommit()) {
+                transactedObject.setMustRestoreAutoCommit(true);
+                con.setAutoCommit(false);
+            }
+
+            transactedObject.getConnectionHolder().setTransactionActive(true);
+
+            if (transactedObject.isNewConnectionHolder())
+                ConnectionHolder.bind(dataSource, transactedObject.getConnectionHolder());
+
+        } catch (Exception e) {
+            DataSourceUtils.releaseConnection(con, this.dataSource);
+            throw new CannotCreateTransactionException("Could not open JDBC Connection for transaction", e);
+        }
+    }
+
+    @Override
+    protected SuspendableResource doSuspend(TransactedObject transactedResource) throws TransactionException {
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.fine("doSuspend");
+        transactedResource.removeConnectionHolder();
+        return ConnectionHolder.unbind(dataSource);
+    }
+
+    @Override
+    protected void doResume(TransactedObject transactedResource, SuspendableResource suspended) throws TransactionException {
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.fine("doResume");
+        ConnectionHolder.bind(dataSource, (ConnectionHolder) suspended);
+    }
+
+    @Override
+    protected void doCommit(TransactedObject transactedResource) throws TransactionException {
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.fine("doCommit");
+        try {
+            transactedResource.getConnectionHolder().getConnection().commit();
+        } catch (SQLException ex) {
+            throw new TransactionSystemException("Could not commit JDBC transaction", ex);
+        }
+    }
+
+    @Override
+    protected void doRollback(TransactedObject transactedResource) throws TransactionException {
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.fine("doRollback");
+        try {
+            transactedResource.getConnectionHolder().getConnection().rollback();
+        } catch (SQLException ex) {
+            throw new TransactionSystemException("Could not commit JDBC transaction", ex);
+        }
+    }
+
+    @Override
+    protected void doCleanup(TransactedObject transactedResource) throws TransactionException {
+        if (LOGGER.isLoggable(Level.FINE))
+            LOGGER.fine("doCleanup");
+        //TODO: not sure - because prevents the ConnectionHolder from being reused and we are using a UnitOfWork already to control connection closing
+        //if (transactedResource.isNewConnectionHolder())
+        //    ConnectionHolder.unbind(dataSource);
+        Connection connection = transactedResource.getConnectionHolder().getConnection();
+        try {
+            if (transactedResource.mustRestoreAutoCommit())
+                connection.setAutoCommit(true);
+            DataSourceUtils.resetConnectionAfterTransaction(connection, transactedResource.getPreviousIsolationLevel());
+        } catch (Throwable ignored) {
+        }
+        if (transactedResource.isNewConnectionHolder() && !transactedResource.getConnectionHolder().isTransactionActive())
+            DataSourceUtils.releaseConnection(connection, this.dataSource);
+        transactedResource.getConnectionHolder().clear();
+    }
+
+}
