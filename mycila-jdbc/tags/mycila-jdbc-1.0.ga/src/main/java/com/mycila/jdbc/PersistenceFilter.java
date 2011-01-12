fe2e3c4c69d3bb319c39670bcc46eb7bb8c49f6b
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2011-01-12 20:16:17 +0000

    [maven-release-plugin]  copy for tag mycila-jdbc-1.0.ga

---
+++ mycila-jdbc/tags/mycila-jdbc-1.0.ga/src/main/java/com/mycila/jdbc/PersistenceFilter.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2010 Mycila <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mycila.jdbc;
+
+import javax.inject.Inject;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+public final class PersistenceFilter implements Filter {
+
+    private final UnitOfWork unitOfWork;
+
+    @Inject
+    public PersistenceFilter(UnitOfWork unitOfWork) {
+        this.unitOfWork = unitOfWork;
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        try {
+            unitOfWork.begin();
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            unitOfWork.end();
+        }
+    }
+}
