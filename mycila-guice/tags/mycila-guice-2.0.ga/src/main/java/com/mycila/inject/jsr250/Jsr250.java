54a95983d55a37d202f1eaf3222da588a6d646c2
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2011-01-12 18:26:24 +0000

    [maven-release-plugin]  copy for tag mycila-guice-2.0.ga

---
+++ mycila-guice/tags/mycila-guice-2.0.ga/src/main/java/com/mycila/inject/jsr250/Jsr250.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (C) 2010 Mycila <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mycila.inject.jsr250;
+
+import com.google.common.collect.Iterables;
+import com.google.inject.Binding;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Module;
+import com.google.inject.Stage;
+import com.google.inject.TypeLiteral;
+import com.google.inject.spi.DefaultElementVisitor;
+import com.google.inject.spi.Element;
+import com.google.inject.spi.Elements;
+import com.mycila.inject.injector.MethodHandler;
+import com.mycila.inject.internal.Reflect;
+
+import javax.annotation.PreDestroy;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+import static com.google.common.collect.Iterables.filter;
+import static com.mycila.inject.internal.Reflect.annotatedBy;
+import static com.mycila.inject.internal.Reflect.findMethods;
+
+/**
+ * @author Mathieu Carbou (mathieu.carbou@gmail.com)
+ */
+public final class Jsr250 {
+    private Jsr250() {
+    }
+
+    /**
+     * Creates an injector for the given set of modules. To create an injector
+     * with a {@link com.google.inject.Stage} or other options, see {@link com.google.inject.InjectorBuilder}.
+     *
+     * @throws com.google.inject.CreationException
+     *          if one or more errors occur during injector
+     *          construction
+     */
+    public static Jsr250Injector createInjector(Module... modules) {
+        return createInjector(Arrays.asList(modules));
+    }
+
+    /**
+     * Creates an injector for the given set of modules. To create an injector
+     * with a {@link com.google.inject.Stage} or other options, see {@link com.google.inject.InjectorBuilder}.
+     *
+     * @throws com.google.inject.CreationException
+     *          if one or more errors occur during injector
+     *          creation
+     */
+    public static Jsr250Injector createInjector(Iterable<? extends Module> modules) {
+        return createInjector(Stage.DEVELOPMENT, modules);
+    }
+
+    /**
+     * Creates an injector for the given set of modules, in a given development
+     * stage. Use {@link com.google.inject.InjectorBuilder} for advanced injector creation.
+     *
+     * @throws com.google.inject.CreationException
+     *          if one or more errors occur during injector
+     *          creation.
+     */
+    public static Jsr250Injector createInjector(Stage stage, Module... modules) {
+        return createInjector(stage, Arrays.asList(modules));
+    }
+
+    /**
+     * Creates an injector for the given set of modules, in a given development
+     * stage. Use {@link com.google.inject.InjectorBuilder} for advanced injector creation.
+     *
+     * @throws com.google.inject.CreationException
+     *          if one or more errors occur during injector
+     *          construction
+     */
+    public static Jsr250Injector createInjector(Stage stage, Iterable<? extends Module> modules) {
+
+        return Guice.createInjector(
+                stage,
+                hasJSR250Module(stage, modules) ? modules : Iterables.concat(modules, Arrays.asList(new Jsr250Module())))
+                .getInstance(Jsr250Injector.class);
+    }
+
+    private static boolean hasJSR250Module(Stage stage, Iterable<? extends Module> modules) {
+        final Key key = Key.get(Jsr250Destroyer.class);
+        for (Element element : Elements.getElements(stage, modules)) {
+            Boolean res = element.acceptVisitor(new DefaultElementVisitor<Boolean>() {
+                @Override
+                public <T> Boolean visit(Binding<T> binding) {
+                    return key.equals(binding.getKey());
+                }
+            });
+            if (res != null && res)
+                return true;
+        }
+        return false;
+    }
+
+    public static Module newJsr250Module() {
+        return new Jsr250Module();
+    }
+
+    public static boolean hasJSR250Module(Injector injector) {
+        return injector.getBindings().containsKey(Key.get(Jsr250Destroyer.class));
+    }
+
+    public static <T> void preDestroy(T instance) {
+        TypeLiteral<T> type = (TypeLiteral<T>) TypeLiteral.get(Reflect.getTargetClass(instance));
+        MethodHandler<PreDestroy> handler = new Jsr250PreDestroyHandler();
+        for (Method method : filter(findMethods(type.getRawType()), annotatedBy(PreDestroy.class))) {
+            handler.handle(type, instance, method, method.getAnnotation(PreDestroy.class));
+        }
+    }
+
+}
---
+++ mycila-guice/tags/mycila-guice-2.0.ga/src/main/java/com/mycila/inject/jsr250/Jsr250Module.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (C) 2010 Mycila <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mycila.inject.jsr250;
+
+import com.google.inject.Binder;
+import com.google.inject.Binding;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Module;
+import com.google.inject.Scope;
+import com.google.inject.Scopes;
+import com.google.inject.Singleton;
+import com.google.inject.spi.BindingScopingVisitor;
+import com.mycila.inject.annotation.Jsr250Singleton;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.mycila.inject.MycilaGuice.in;
+
+/**
+ * @author Mathieu Carbou (mathieu.carbou@gmail.com)
+ */
+final class Jsr250Module implements Module {
+
+    private final Set<Scope> scopes = new HashSet<Scope>();
+    private final Set<Class<? extends Annotation>> scopeAnnotations = new HashSet<Class<? extends Annotation>>();
+
+    @Override
+    public void configure(Binder binder) {
+        binder.bind(Jsr250Injector.class).to(Jsr250InjectorImpl.class).in(Singleton.class);
+        in(binder)
+                .bindAnnotationInjector(Resource.class, Jsr250KeyProvider.class)
+                .handleMethodAfterInjection(PostConstruct.class, Jsr250PostConstructHandler.class)
+                .bind(Jsr250Destroyer.class, new Jsr250Destroyer() {
+                    @Inject
+                    Injector injector;
+
+                    @Override
+                    public void preDestroy() {
+                        BindingScopingVisitor<Boolean> visitor = new BindingScopingVisitor<Boolean>() {
+                            @Override
+                            public Boolean visitEagerSingleton() {
+                                return true;
+                            }
+
+                            @Override
+                            public Boolean visitScope(Scope scope) {
+                                return scope.getClass().isAnnotationPresent(Jsr250Singleton.class)
+                                        || scopes.contains(scope);
+                            }
+
+                            @Override
+                            public Boolean visitScopeAnnotation(Class<? extends Annotation> scopeAnnotation) {
+                                return scopeAnnotation.isAnnotationPresent(Jsr250Singleton.class)
+                                        || scopeAnnotations.contains(scopeAnnotation);
+                            }
+
+                            @Override
+                            public Boolean visitNoScoping() {
+                                return false;
+                            }
+                        };
+                        for (Binding<?> binding : injector.getAllBindings().values())
+                            if (Scopes.isSingleton(binding) || binding.acceptScopingVisitor(visitor))
+                                Jsr250.preDestroy(binding.getProvider().get());
+                        for (Scope scope : injector.getScopeBindings().values())
+                            Jsr250.preDestroy(scope);
+                    }
+                });
+    }
+
+    public Jsr250Module addCloseableScopes(Scope... scopes) {
+        this.scopes.addAll(Arrays.asList(scopes));
+        return this;
+    }
+
+    public Jsr250Module addCloseableScopes(Class<? extends Annotation>... scopeAnnotations) {
+        this.scopeAnnotations.addAll(Arrays.asList(scopeAnnotations));
+        return this;
+    }
+
+}
