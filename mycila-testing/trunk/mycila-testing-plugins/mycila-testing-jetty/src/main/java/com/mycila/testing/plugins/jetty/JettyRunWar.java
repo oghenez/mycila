4948110fe683907cb4c4ec8338d17a0f15491ba9
amertum <amertum@gmail.com>
2011-01-17 17:25:12 +0000

    add serverLifeCycleListener option property to JettyRunWar annotation

--- mycila-testing/trunk/mycila-testing-plugins/mycila-testing-jetty/src/main/java/com/mycila/testing/plugins/jetty/JettyRunWar.java
+++ mycila-testing/trunk/mycila-testing-plugins/mycila-testing-jetty/src/main/java/com/mycila/testing/plugins/jetty/JettyRunWar.java
@@ -16,9 +16,6 @@
 
 package com.mycila.testing.plugins.jetty;
 
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
@@ -26,9 +23,14 @@ import java.lang.annotation.Target;
 
 import com.mycila.testing.junit.MycilaJunitRunner;
 
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 /**
- * This annotation enables the loading of a packaged web application (ie: a WAR file) by a Servlet Container when used
- * with junit runner {@link org.junit.runner.RunWith} and {@link MycilaJunitRunner}. This is useful to run functional
+ * This annotation enables the loading of a packaged web application (ie: a WAR file) by a Servlet
+ * Container when used
+ * with junit runner {@link org.junit.runner.RunWith} and {@link MycilaJunitRunner}. This is useful
+ * to run functional
  * tests.
  * <p>
  * Here's an example which run webapp accessible by default at URL : {@code http://localhost:9090}
@@ -37,6 +39,7 @@ import com.mycila.testing.junit.MycilaJunitRunner;
  * &#064;RunWith(MycilaJunitRunner.class)
  * &#064;JettyRunWar
  * public class ExampleTest {
+ *     
  *     &#064;Test
  *      public void testSomething() {
  *          ...
@@ -58,7 +61,7 @@ import com.mycila.testing.junit.MycilaJunitRunner;
 @Inherited
 @Documented
 public @interface JettyRunWar {
-
+    
     /**
      * Alias for {@link #war()}.
      * 
@@ -67,21 +70,22 @@ public @interface JettyRunWar {
      * @see #war()
      */
     String value() default "";
-
+    
 
     /**
      * The path of the WAR file to load. The search is based on the current directory.
      * <p>
-     * If the default value "" is used, a search of all WAR file based on the current directory will be done. If there
-     * is more than one WAR file the test will fail, else the WAR file will be loaded and the test run.
+     * If the default value "" is used, a search of all WAR file based on the current directory will
+     * be done. If there is more than one WAR file the test will fail, else the WAR file will be
+     * loaded and the test run.
      * <p>
      * WAR locator strategy :
      * <ul>
      * <li>default path is either relative or absolute
      * <li>starts with "reg:" to enable the following to be java regular expression for this path
      * <ul>
-     * <li>the regular expression should starts with the current directory {@code './'}, which translated in java
-     * pattern is {@code '\\.\\/'}
+     * <li>the regular expression should starts with the current directory {@code './'}, which
+     * translated in java pattern is {@code '\\.\\/'}
      * </ul>
      * <li>starts with "ant:" to enable the following to be ant path expression for this path, ie:
      * &#42;&#42;/webapp-*.war
@@ -96,16 +100,17 @@ public @interface JettyRunWar {
      * @return path of the WAR file to load.
      */
     String war() default "";
-
+    
 
     /**
-     * The web application context path. Must starts with a slash '/' but doesn't end with one except for root context
+     * The web application context path. Must starts with a slash '/' but doesn't end with one
+     * except for root context
      * path.
      * 
      * @return the web application context path.
      */
     String contextPath() default "/";
-
+    
 
     /**
      * The web application server port.
@@ -123,8 +128,14 @@ public @interface JettyRunWar {
     TargetWebapp targetWebapp() default TargetWebapp.AUTO_RUN;
     
 
-    // TODO Class<Config> config(); LifeCycleListener before, after
+    /**
+     * The server lifecycle listener which allow customization of the server configuration.
+     * 
+     * @return the server lifecycle listener.
+     */
+    Class<? extends ServerLifeCycleListener> serverLifeCycleListener() default NopServerLifeCycleListener.class;
     
+
     public enum TargetWebapp {
         /**
          * Auto run the webapp before lauching the test.
