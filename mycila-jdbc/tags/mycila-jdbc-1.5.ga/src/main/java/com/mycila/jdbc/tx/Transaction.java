9db4e16cfb85e6259b068da2e114df431e6f19a0
mathieu.carbou <mathieu.carbou@c2eb063a-a55a-11dd-a77a-2d921c902ce3>
2011-11-17 19:02:22 +0000

    [maven-release-plugin]  copy for tag mycila-jdbc-1.5.ga

---
+++ mycila-jdbc/tags/mycila-jdbc-1.5.ga/src/main/java/com/mycila/jdbc/tx/Transaction.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2010 Mycila <mathieu.carbou@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.mycila.jdbc.tx;
+
+/**
+ * Representation of the status of a transaction.
+ * <p/>
+ * <p>Transactional code can use this to retrieve status information,
+ * and to programmatically request a rollback (instead of throwing
+ * an exception that causes an implicit rollback).
+ * <p/>
+ * <p>Derives from the SavepointManager interface to provide access
+ * to savepoint management facilities. Note that savepoint management
+ * is only available if supported by the underlying transaction manager.
+ */
+public interface Transaction<T extends Transacted> {
+
+    TransactionDefinition getDefinition();
+
+    boolean hasTransactedResource();
+
+    T getTransactedResource();
+
+    boolean hasSuspendedResource();
+
+    SuspendableResource getSuspendedResource();
+
+    /**
+     * Return whether the present transaction is new (else participating
+     * in an existing transaction, or potentially not running in an
+     * actual transaction in the first place).
+     */
+    boolean isNew();
+
+    boolean isCompleted();
+
+    void commit() throws TransactionException;
+
+    void rollback() throws TransactionException;
+
+}
